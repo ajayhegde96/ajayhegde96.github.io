f5b9e9f6a94ec3b26579f7ffe7fbee75aa9dde33
Ajay Hegde <ajayhegde97@gmail.com>
2022-08-29 02:41:24 -0400

    Updates

---
+++ precache-manifest.cf7079d6f8fffcfeb8ba81911795b37f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffeec136f4b801fdeec1",
+    "url": "/static/css/main.26a7452f.chunk.css"
+  },
+  {
+    "revision": "ffeec136f4b801fdeec1",
+    "url": "/static/js/main.ffeec136.chunk.js"
+  },
+  {
+    "revision": "4a513ad34b9c808eb1a9",
+    "url": "/static/js/1.4a513ad3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7457b38c2abf04eeb3b0104a1cb6c1b0",
+    "url": "/index.html"
+  }
+];
